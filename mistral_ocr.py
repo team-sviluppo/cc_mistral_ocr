16bac1a92f887ccc587c7778582f77c1ac5a35a7
Cristiano Revil <49713716+cristianorevil@users.noreply.github.com>
2025-06-25 09:24:35 +0200

    update version 1.0.1

--- mistral_ocr.py
+++ mistral_ocr.py
@@ -4,17 +4,19 @@ from datetime import timedelta
 from cat.mad_hatter.decorators import hook
 from cat.mad_hatter.decorators import endpoint
 from pydantic import BaseModel
+from fastapi import UploadFile, File
 from cat.auth.permissions import AuthResource, AuthPermission, check_permissions
 import json
 import base64
 import requests
 import os
+import tempfile
 from mistralai import Mistral
 
 
 class Tag(BaseModel):
     name: str
-    value: str
+    value: List[str]
 
 
 class OCRInput(BaseModel):
@@ -54,3 +56,66 @@ def ocr(
             cat.rabbit_hole.ingest_file(cat, output_file, 400, 100, metadata)
             os.remove(output_file)
     return ocr_response
+
+
+@endpoint.post("/ocr-pdf")
+async def ocr_pdf(
+    file: UploadFile = File(...),
+    tags: List[Tag] = [],
+    cat=check_permissions("CONVERSATION", "WRITE"),
+) -> str:
+
+    original_filename = file.filename
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    api_key = settings["mistral_api_key"]
+    save_rh = settings["save_text_to_rabbit_hole"]
+    client = Mistral(api_key=api_key)
+
+    # Salviamo il file PDF temporaneamente
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_pdf:
+        temp_pdf_path = temp_pdf.name
+        # Leggiamo il file caricato in blocchi per gestire file di grandi dimensioni
+        content = await file.read()
+        temp_pdf.write(content)
+
+    try:
+        # Processare il PDF con Mistral OCR
+        ocr_response = client.ocr.process(
+            model="mistral-ocr-latest",
+            document={
+                "type": "document_url",
+                "document_url": upload_pdf(temp_pdf_path),
+            },
+            include_image_base64=True,
+        )
+
+        log.debug(f"OCR PDF response: {ocr_response}")
+
+        if save_rh:
+            for i, page in enumerate(ocr_response.pages):
+                # Nome del file di output
+                output_file = f"{original_filename}_{i}.md"
+                with open(output_file, "w", encoding="utf-8") as f:
+                    f.write(page.markdown)
+                metadata = {item.name: item.value for item in tags}
+                cat.rabbit_hole.ingest_file(cat, output_file, 512, 128, metadata)
+                os.remove(output_file)
+
+        return ocr_response
+
+    finally:
+        # Pulizia: rimuovere il file temporaneo
+        if os.path.exists(temp_pdf_path):
+            os.remove(temp_pdf_path)
+
+
+def upload_pdf(client, filename):
+    uploaded_pdf = client.files.upload(
+        file={
+            "file_name": filename,
+            "content": open(filename, "rb"),
+        },
+        purpose="ocr",
+    )
+    signed_url = client.files.get_signed_url(file_id=uploaded_pdf.id)
+    return signed_url.url
